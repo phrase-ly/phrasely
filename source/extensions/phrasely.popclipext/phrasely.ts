7fe4a1a338d90e03c434a5bf40e1a67255e9d724
Patrick Düggelin <dueggelin@textshuttle.ai>
2023-09-16 01:35:49 +0200

    :recycle: Restructure to module

--- source/extensions/phrasely.popclipext/phrasely.ts
+++ source/extensions/phrasely.popclipext/phrasely.ts
@@ -1,58 +1,6 @@
-import axios from "axios";
-import {getErrorInfo} from "../ChatGPT.popclipext/chatgpt";
-import {re} from "mathjs";
+import generateImage from "./image";
 
 
-interface Image {
-  url: string
-}
-
-interface ImageResponse {
-  created: Number
-  data: Image[]
-}
-
-
-const SUPPORTS_HTML = ["Microsoft Word"]
-
-const generateImage: ActionFunction = async (input, options) => {
-  const openai = axios.create({
-    baseURL: "https://api.openai.com/v1",
-    headers: { Authorization: `Bearer ${options.apikey}` },
-    timeout: 60_000
-  });
-
-  // send the whole message history to OpenAI
-  try {
-    const response = await openai.post(
-        "/images/generations",
-        {
-          "prompt": input.text,
-          "n": 1, // Todo move to options
-          "size": "256x256" // Todo move to options
-        },
-    );
-
-
-    const { data } = response
-    // popclip.showText(data.data[0].url, {preview: true})
-    const imageHTML = `<img src=${data.data[0].url} alt=${input.text}/>`
-    const linkHTML = `<a href="${data.data[0].url}" >${input.text}</a>`
-    const appName = popclip.context.appName
-    popclip.showText(appName)
-    if (SUPPORTS_HTML.includes(appName)) {
-      popclip.pasteContent({"public.html": imageHTML, "public.utf8-plain-text": input.text});
-    }
-    else {
-      popclip.pasteContent({"public.html": linkHTML, "public.utf8-plain-text": input.text});
-    }
-    popclip.showSuccess();
-  } catch (e) {
-    // @ts-ignore
-    popclip.showText(e.toString());
-  }
-  return null;
-};
 
 // export the actions
 export const actions: Action[] = [{
