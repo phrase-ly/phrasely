d52c9e30ca07df9aab1fec15765d8f60f317e231
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2023-09-16 13:51:19 +0200

    audio script

--- google_text2speech/play_audio.sh
+++ google_text2speech/play_audio.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Check if an argument (MP3 file path) is provided
+# Check if an argument (URL) is provided
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <MP3_FILE>"
+  echo "Usage: $0 <URL>"
   exit 1
 fi
 
-# Get the MP3 file path from the first argument
-mp3_file="$1"
+# Get the URL from the first argument
+url="$1"
 
-# Check if the MP3 file exists
-if [ ! -f "$mp3_file" ]; then
-  echo "Error: The specified MP3 file does not exist."
+# Generate a temporary filename for the downloaded MP3 file
+tmp_file=$(mktemp /tmp/downloaded_mp3_XXXXXX.mp3)
+
+# Use curl to download the MP3 file from the provided URL
+curl -o "$tmp_file" "$url"
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to download the MP3 file from $url."
   exit 1
 fi
 
-# Use afplay to play the MP3 file
-afplay "$mp3_file"
+# Use afplay to play the downloaded MP3 file
+afplay "$tmp_file"
+
+# Clean up the temporary file
+rm -f "$tmp_file"
