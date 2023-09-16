a8bcd986e90b6ad4d7afaa2fdbf75c165cc536a4
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2023-09-16 17:24:47 +0200

    amazing script :poop:

--- source/extensions/phrasely-speech.popclipext/play_audio.sh
+++ source/extensions/phrasely-speech.popclipext/play_audio.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
 # Check if an argument (URL) is provided
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
+# if [ $# -eq 0 ]; then
+#   echo "Usage: $0 <URL>"
+#   exit 1
+# fi
 
 # Get the URL from the first argument
-url="$1"
+text="$(</dev/stdin)"
+
+language=$(curl -X 'POST' \
+  'https://traefik.stg.open.textshuttle.com/api/unstable/v4/detect_language' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "text": "${text}"
+}')
+
+language=$(echo $language | grep -o '"language": *"[^"]*"' | grep -o '"[^"]*"$')
 
 # Generate a temporary filename for the downloaded MP3 file
-tmp_file=$(mktemp /tmp/downloaded_mp3_XXXXXX.mp3)
+# tmp_file=$(mktemp /tmp/downloaded_mp3_XXXXXX.mp3)
 
-# Use curl to download the MP3 file from the provided URL
-curl -o "$tmp_file" "$url"
+# # Use curl to download the MP3 file from the provided URL
 
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to download the MP3 file from $url."
-  exit 1
-fi
+# encoded_value=$(python3 -c "import urllib.parse; print urllib.parse.quote('''$text''')")
+# lang=$(python3 -c "import urllib.parse; print urllib.parse.quote('''$lang''')")
+res=$(curl  -G \
+    https://us-central1-hackzurich23-8237.cloudfunctions.net/text2speech \
+    --data-urlencode "text=$text" \
+    --data-urlencode "lang=$lang" \
+    --http1.1\
+    -v\
+    -o /tmp/zium.mp3
+    )
+# curl--data-urlencode "blah=df ssdf sdf" --data-urlencode "blah2=dfsdf sdfsd " http://whatever.com/whatever
+echo "Zium " $res
+# # Check if the download was successful
+# if [ $? -ne 0 ]; then
+#   echo "Error: Failed to download the MP3 file from $url."
+#   exit 1
+# fi
 
 # Use afplay to play the downloaded MP3 file
-afplay "$tmp_file"
+afplay "/tmp/zium.mp3"
 
 # Clean up the temporary file
 rm -f "$tmp_file"
--- source/extensions/phrasely-speech.popclipext/text_to_speech.ts
+++ source/extensions/phrasely-speech.popclipext/text_to_speech.ts
@@ -2,20 +2,33 @@ import axios from "axios";
 
 // the main chat action
 // @ts-ignore 
-const text_to_speech: ActionFunction = async (input, options) => {
-    let language = ""
-    await axios.post("https://traefik.stg.open.textshuttle.com/api/unstable/v4/detect_language", JSON.parse(input.text)).then((response) => 
-    {
-        language = response.data
-    });
-
+const text_to_speech = async (input, options) => {
+    let language = "";
+    url_.searchParams.append("text", input)
+    const response = await axios.post(
+        'https://traefik.stg.open.textshuttle.com/api/unstable/v4/detect_language',
+        // '{\n  "text": "Winston ging die Treppe hinauf."\n}',
+        {
+          'text': input
+        },
+        {
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+    language = response.data.language
+    // await axios.post(url_.toString())
+    console.log(language)
     let url = new URL("https://us-central1-hackzurich23-8237.cloudfunctions.net/text2speech")
-    url.searchParams.append("text", input.text)
+    url.searchParams.append("text", input)
     url.searchParams.append("lang", language)
-// 
+// // 
     // shell.exec('./play_audio.sh', url)
+    // await exec('bash /Users/jakubkaczmarski/phrasely/source/extensions/phrasely-speech.popclipext/play_audio.sh', url.toString(), () => {})
     const { exec } = require('child_process');
-    exec('bash ./play_audio.sh', url, (err, stdout, stderr) => {
+    exec('bash ./play_audio.sh ' + url.toString(), (err, stdout, stderr) => {
     if (err) {
         //some err occurred
         console.error(err)
@@ -25,10 +38,12 @@ const text_to_speech: ActionFunction = async (input, options) => {
     console.log(`stderr: ${stderr}`);
     }
     });
+    console.log("Played the audio plz", url.toString())
 }
 
+text_to_speech("Hi how's it goingHi how's it goingHi how's it goingHi how's it  going Hi how's it goingHi how's it goingHi how's it goingHi how's it  going ", "")
 // export the actions
-// @ts-ignore 
+//@ts-ignore
 export const actions: Action[] = [{
   title: "TextoSpeech",
   code: text_to_speech,
